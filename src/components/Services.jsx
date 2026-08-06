4296bedb0073f9ca335fdfd9dfd137767f88aad6
PriyankGupta22 <gsanu8092@gmail.com>
2026-08-07 04:18:50 +0530

    added more pictures

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,19 +3,25 @@ import { FaArrowRight } from "react-icons/fa";
 import service1 from "../assets/services1.webp"
 import service2 from "../assets/services2.webp"
 import service3 from "../assets/services3.webp"
+import service4 from "../assets/services4.webp"
+import service5 from "../assets/services5.webp"
+import service6 from "../assets/services6.webp"
 
-const MainContent = ({title, number, content}) => {
+
+const MainContent = ({title, number, content, image}) => {
     return (
-    <div className = "h-[370px] border border-[0.5px] border-white/20 flex flex-row items-center border-x-black border-t-black px-[90px]">
-        <div className = "flex felx-row justify-center">
+    <div className = "h-[370px] border border-[0.5px] border-white/20 flex flex-row items-center border-x-black border-t-black px-[90px] relative">
+        <img src={image} alt="" className = "absolute inset-0 h-full w-full object-cover"/>
+        <div className = "h-full w-full absolute bg-black opacity-70 left-0"></div>
+        <div className = "flex felx-row justify-center z-10">
             <div className = "border border-white/60 h-[45px] w-[45px] flex rounded-full items-center justify-center text-white text-[18px] mb-[200px] border-[2px] text-semibold font-geist">{number}</div>
             <div className = "flex flex-wrap w-[480px] h-max text-white text-[21px] ml-[70px] mb-[50px] pr-[140px]">{content}</div>
         </div>
         
-        <div className = "w-[800px] flex flex-col text-white text-[70px] font-geist mt-[20px] text-[180px] mb-[70px] ml-[40px]">
+        <div className = "w-[800px] flex flex-col text-white text-[70px] font-geist mt-[20px] text-[180px] mb-[70px] ml-[40px] z-10">
         {title}
         </div>
-        <div className = "text-white text-[180px] mb-[40px]"><FaArrowRight /></div>
+        <div className = "text-white text-[180px] mb-[40px] z-10"><FaArrowRight /></div>
     </div>
     )
 }
@@ -42,9 +48,9 @@ const Services = () => {
         <div className = "mt-[-35px]">Can Help You</div></div>
       </div>
       <div className = "flex flex-col">
-        <MainContent number = "1" title = "Buy" content = "Buy smarter with expert agents backed by mortgage, legal, and appraisal pros—dialed in to get you the best deal, fast. We've done this over 10,000 times, and we know what wins."/>
-        <MainContent number = "2" title = "Sell" content = "Sell fast, sell high. Your listing gets pro staging, strategic pricing, constant open houses, and agents who never stop working until the right buyer signs."/>
-        <MainContent number = "3" title = "Rent" content = "Access hidden rentals before they hit the market through agents who know every landlord in town. With decades of NYC experience, we unlock the best deals you won't find online."/>
+        <MainContent number = "1" title = "Buy" content = "Buy smarter with expert agents backed by mortgage, legal, and appraisal pros—dialed in to get you the best deal, fast. We've done this over 10,000 times, and we know what wins." image = {service4}/>
+        <MainContent number = "2" title = "Sell" content = "Sell fast, sell high. Your listing gets pro staging, strategic pricing, constant open houses, and agents who never stop working until the right buyer signs." image = {service5}/>
+        <MainContent number = "3" title = "Rent" content = "Access hidden rentals before they hit the market through agents who know every landlord in town. With decades of NYC experience, we unlock the best deals you won't find online." image = {service6}/>
       </div>
       <div className = "h-[340px] flex flex-col justify-center px-[90px]">
         <div className = "w-[720px] text-white font-geist text-[41px] leading-11">Our certified agents guide you through every stage of real estate with expert knowledge and reliable support.</div>
